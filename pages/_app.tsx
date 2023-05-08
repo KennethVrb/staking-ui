f49500b2ec6cd6c45e68cc15e83234f73ef54830
Kenneth Verbeure <kenneth.verbeure@biscglobal.com>
2023-05-08 22:00:51 +0200

    initial

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,48 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { Container, Header, Segment, Grid } from "semantic-ui-react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import "semantic-ui-css/semantic.min.css";
+import "./index.css"; // Import the custom CSS file
+
+const Layout: React.FC = ({ children }) => {
+  return (
+    <div
+      style={{ display: "flex", minHeight: "100vh", flexDirection: "column" }}
+    >
+      <Segment inverted vertical textAlign="center" padded color="blue">
+        <Header as="h1" inverted>
+          Polkadot Validator Dashboard
+        </Header>
+      </Segment>
+      <Container style={{ flex: 1, paddingTop: "2em", paddingBottom: "2em" }}>
+        {children}
+      </Container>
+      <Segment vertical style={{ padding: "2em 0em" }}>
+        <Container textAlign="center">
+          <Grid divided inverted stackable>
+            <Grid.Row>
+              <Grid.Column>
+                <Header as="h4" content="Polkadot Validator Dashboard" />
+                <p>
+                  © {new Date().getFullYear()} All rights reserved. Polkadot
+                  Validator Dashboard is not affiliated with or endorsed by
+                  Polkadot.
+                </p>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Container>
+      </Segment>
+    </div>
+  );
+};
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
+
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,192 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import React from "react";
+import {
+  Container,
+  Table,
+  Pagination,
+  Dropdown,
+  Dimmer,
+  Loader,
+  Popup,
+} from "semantic-ui-react";
+import { useEffect, useState } from "react";
+import { ApiPromise, WsProvider } from "@polkadot/api";
+import { IdentityInfo } from "@polkadot/types/interfaces";
+import { formatBalance } from "@polkadot/util";
 
-const inter = Inter({ subsets: ['latin'] })
+type Validator = {
+  validator: string;
+  stashId: string;
+  commission: string;
+  slashEvents: number;
+  totalBonded: string;
+  displayName?: string;
+};
 
-export default function Home() {
+const ValidatorDashboard = () => {
+  const [validators, setValidators] = useState<Validator[]>([]);
+  const [activePage, setActivePage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(15);
+  const [loading, setLoading] = useState(true);
+
+  const itemsPerPageOptions = [
+    { key: 1, text: "15", value: 15 },
+    { key: 2, text: "25", value: 25 },
+    { key: 3, text: "50", value: 50 },
+    { key: 4, text: "100", value: 100 },
+  ];
+
+  const truncateAddress = (address: string, length: number = 10) => {
+    return `${address.slice(0, length)}...${address.slice(-length)}`;
+  };
+
+  const handlePaginationChange = (e, { activePage }) => {
+    setActivePage(activePage);
+  };
+
+  const handleItemsPerPageChange = (e, { value }) => {
+    setItemsPerPage(value);
+    setActivePage(1); // Reset to the first page
+  };
+
+  const totalPages = Math.ceil(validators.length / itemsPerPage);
+
+  useEffect(() => {
+    const fetchValidators = async () => {
+      setLoading(true);
+      const provider = new WsProvider("wss://rpc.polkadot.io");
+      const api = await ApiPromise.create({ provider });
+
+      const validatorKeys = await api.query.session.validators();
+      const validatorStashKeys = await api.query.staking.bonded.multi(
+        validatorKeys
+      );
+      const identityInfos =
+        await api.query.identity.identityOf.multi<IdentityInfo>(
+          validatorStashKeys
+            .map((stash) => stash.unwrapOr(null))
+            .filter((stash) => stash)
+        );
+
+      const decimals = await api.registry.chainDecimals;
+
+      const validatorInfo = await Promise.all(
+        validatorKeys.map(async (validator, index) => {
+          const stashId =
+            validatorStashKeys[index].unwrapOr(null)?.toString() ?? "";
+          const { commission } = await api.query.staking.validators(validator);
+          const slashEvents =
+            (await api.query.staking.slashingSpans(validator)).unwrapOr(null)
+              ?.prior.length ?? 0;
+          const totalBondedPlanck = (await api.query.staking.ledger(validator))
+            .unwrapOr(null)
+            ?.active.toBn();
+
+          const totalBondedDot = totalBondedPlanck
+            ? formatBalance(totalBondedPlanck, {
+                forceUnit: "-",
+                decimals: decimals[0],
+              })
+            : "0";
+
+          let displayName = "";
+          const identityInfo = identityInfos[index]?.unwrapOr(null);
+          if (identityInfo && identityInfo.info.display) {
+            displayName = identityInfo.info.display.toHuman().Raw;
+          }
+
+          return {
+            validator: validator.toString(),
+            stashId,
+            commission: commission.toHuman(),
+            slashEvents,
+            totalBonded: `${totalBondedDot.split(" ")[0]} DOT`,
+            displayName,
+          };
+        })
+      );
+
+      setValidators(validatorInfo);
+      setLoading(false);
+    };
+
+    fetchValidators();
+  }, []);
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <Container>
+      <Dimmer active={loading} inverted>
+        <Loader size="large">Loading</Loader>
+      </Dimmer>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          marginBottom: "1em",
+        }}
+      >
+        <Dropdown
+          selection
+          options={itemsPerPageOptions}
+          value={itemsPerPage}
+          onChange={handleItemsPerPageChange}
+        />
+        <Pagination
+          activePage={activePage}
+          onPageChange={handlePaginationChange}
+          totalPages={totalPages}
+          siblingRange={1}
         />
       </div>
+      <Table celled structured compact unstackable>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Validator Address</Table.HeaderCell>
+            <Table.HeaderCell>Commission</Table.HeaderCell>
+            <Table.HeaderCell>Slash Events</Table.HeaderCell>
+            <Table.HeaderCell>Total Bonded</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        <Table.Body>
+          {validators
+            .slice((activePage - 1) * itemsPerPage, activePage * itemsPerPage)
+            .map((validator, index) => (
+              <Table.Row key={index}>
+                <Table.Cell>
+                  {validator.displayName ? (
+                    <>
+                      <Popup
+                        content={validator.validator}
+                        trigger={
+                          <span>
+                            {validator.displayName}
+                            <br />
+                            <small>
+                              {truncateAddress(validator.validator)}
+                            </small>
+                          </span>
+                        }
+                      />
+                    </>
+                  ) : (
+                    <Popup
+                      content={validator.validator}
+                      trigger={
+                        <span>{truncateAddress(validator.validator)}</span>
+                      }
+                    />
+                  )}
+                </Table.Cell>
+                <Table.Cell>{validator.commission}</Table.Cell>
+                <Table.Cell>{validator.slashEvents}</Table.Cell>
+                <Table.Cell>{validator.totalBonded}</Table.Cell>
+              </Table.Row>
+            ))}
+        </Table.Body>
+      </Table>
+    </Container>
+  );
+};
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default ValidatorDashboard;
